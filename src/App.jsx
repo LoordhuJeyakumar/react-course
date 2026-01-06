0d9b5c5d8ae9405f75a28a08c2b53f01bb59a4eb
Loordhu Jeyakumar <loordhujeyakumar@gmail.com>
2026-01-06 13:16:59 +0530

    feat: Refactor App component to implement user login status and display hobbies

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,79 @@
 import React from "react";
-import UserCard from "./components/UserCard";
-import "./App.css";
-import NavBar from "./components/NavBar";
-import { mockUsers } from "./data/mockData";
-import Card from "./components/Card";
+
+// Conditional rendering example
+
+const user = {
+  isLoggedIn: true,
+  data: { name: "John Doe" },
+};
+
+// rendering list based on login status and conditional rendering list
+
+const hobbies = [
+  {
+    name: "Reading",
+    isFaviroute: true,
+  },
+  {
+    name: "Traveling",
+    isFaviroute: false,
+  },
+  {
+    name: "Cooking",
+    isFaviroute: true,
+  },
+  {
+    name: "Gaming",
+    isFaviroute: false,
+  },
+  {
+    name: "Hiking",
+    isFaviroute: true,
+  },
+  {
+    name: "Swimming",
+    isFaviroute: false,
+  },
+  {
+    name: "Coding",
+    isFaviroute: true,
+  },
+];
 
 function App() {
-
-  //Use State example
-const [user, setUser] = React.useState({
-    name: "Jane Smith",
-    role: "Designer",
-    description: "Jane is a creative designer with a passion for user experience."
-  });
-
-  const getRandomUser = () => {
-  const data = mockUsers;
-  const randomIndex = Math.floor(Math.random() * data.length);
-  return data[randomIndex];
-  
-  }
-
-  const handleChangeUser = () => {
-    const randomUser = getRandomUser();
-    setUser(randomUser);
-  }
-  
-
-
-
-
+  const isLoggedIn = user.isLoggedIn;
+  console.log(isLoggedIn);
   return (
-    <div className="">
-      <NavBar />
-      <UserCard user={user} />
-      <button className="btn btn-primary" onClick={handleChangeUser}>Change User</button>
-    <Card title="Generic Card" heading="Card Header" footerContent="Card Footer">
-      <p>This is a generic card component wrapping some content.</p>
-    </Card>
+    <div>
+      <div>
+        {isLoggedIn ? (
+          <h1>Welcome back, {user.data.name}!</h1>
+        ) : (
+          <h1>Please log in to continue.</h1>
+        )}
+      </div>
+      {isLoggedIn && (
+        <div>
+          <h2>Your Hobbies:</h2>
+          <h3>Faviroute Hobbies:</h3>
+          <ul>
+            {hobbies
+              .filter((hobby) => hobby.isFaviroute)
+              .map((hobby, index) => (
+                <li key={index}>{hobby.name}</li>
+              ))}
+          </ul>
+
+          <h3>Other Hobbies:</h3>
+          <ul>
+            {hobbies
+              .filter((hobby) => !hobby.isFaviroute)
+              .map((hobby, index) => (
+                <li key={index}>{hobby.name}</li>
+              ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
